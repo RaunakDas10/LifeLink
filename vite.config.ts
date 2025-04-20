df5022a23e4f4064b2f9e937fd2dbe4ffbc569bc
Priya Mahato <mahatopriya004@gmail.com>
2025-04-20 11:49:04 +0530

    Fix package.json and add deployment scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LifeLink/',
+  base: '/LifeLink',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
